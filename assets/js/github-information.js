2b2754ae5bbeef91c6199eef81d29a7821902c04
thomasmcquillan <thomas.mcquillan@gmail.com>
2021-10-01 16:43:36 +0000

    Add when then function to retrieve github username

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,7 +8,24 @@ function fetchGitHubInformation(event) {
     
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..." />
-            </div>`);
-            
+            <img src="assets/images/loader.gif" alt="loading..." />
+        </div>`);
+    
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
